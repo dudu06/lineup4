ac13184aff51e0060c6acb96ed6e0b8319ab2e1c
dudu06 <jeremidudu@gmail.com>
2014-12-04 00:43:35 -0800

    Created helper method that shows correct amount of followers and followees for barber and customer.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,12 @@ module ApplicationHelper
     object.likers(Customer).count + object.likers(Barber).count
   end
 
+  def get_follows(object)
+  	object.followers(Customer).count + object.followers(Barber).count
+  end
+
+  def get_fans(object)
+  	object.followables(Customer).count + object.followables(Barber).count
+  end
+
 end
