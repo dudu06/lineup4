a04230570bfa0384bc1db3a58082332601d73af2
dudu06 <jeremidudu@gmail.com>
2015-01-07 18:05:42 -0800

    Fixed heroku stuff again.

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -26,36 +26,3 @@ end
 
 
 
-
-$ heroku rake db:create
-heroku run rake
-heroku open
-bundle update
-git push heroku master
-heroku run rake db:migrate
-bundle install
-git push heroku master
-git add -A
-git commit -m "remove gem"
-git push origin heroku master
-git push heroku master
-git status
-
-git checkout master
-git stash
-rails s
-git checkout new
-git merge -s ours master
-git checkout master
-git merge new
-git add -A
-git commit -m"remove gem test"
-git push heroku master
- heroku apps:rename line-up
- heroku run rails console 
- $ heroku run -a line-up console
-heroku logs
-heroku logs --source app
-
-
-vim config fucked me up when I removed every line referencing heroku!
