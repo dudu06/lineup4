a8bf26bd7e99a45dae830bb60cd087a9e74ab701
dudu06 <jeremidudu@gmail.com>
2015-01-08 11:39:47 -0800

    Still trying to set up S3

--- app/models/barber.rb
+++ app/models/barber.rb
@@ -7,7 +7,7 @@ class Barber < ActiveRecord::Base
   has_many :comments
 	has_many :portfolios
 	has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png", :storage =>   
-:s3, :bucket => "lineupbucket"
+:s3, :bucket => "lineupnewbucket"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
   	def self.search(search)
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -6,7 +6,7 @@ class Customer < ActiveRecord::Base
 	has_secure_password 
 	has_many :comments
 	has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png", :storage =>   
-:s3, :bucket => "lineupbucket"
+:s3, :bucket => "lineupnewbucket"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
   def pretty
