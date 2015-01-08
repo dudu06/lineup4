06ccd8761e91270b63f3c4dbda3ad1194e195ec0
dudu06 <jeremidudu@gmail.com>
2015-01-08 11:17:49 -0800

    still yet trying to get S3 to work with heroku.

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -5,8 +5,8 @@ class Customer < ActiveRecord::Base
     # acts_as_voter
 	has_secure_password 
 	has_many :comments
-	has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/thumb/missing.png", :storage =>   
-:s3, :bucket => "<lineupbucket>"
+	has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png", :storage =>   
+:s3, :bucket => "lineupbucket"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
   def pretty
